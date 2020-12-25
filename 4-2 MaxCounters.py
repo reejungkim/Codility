4dc0e7ecfcd54625b4b09b0763d2f42ab74d83f7
reejung kim <reejung.kim@gmail.com>
2020-12-25 15:36:41 +0000

    Edit to get better complexity O(N*M)

--- 4-2 MaxCounters.py
+++ 4-2 MaxCounters.py
@@ -5,23 +5,42 @@ Created on Fri Dec 25 14:40:04 2020
 
 @author: reejungkim
 """
+# =============================================================================
+#     
+#     def solution(N, A):
+#     # write your code in Python 3.6
+#     
+#     
+#     arr = [0]*N
+#     
+#     
+#     for i in A:
+#         if i <= N:
+#             arr[i-1] = arr[i-1]+1
+#        
+#         else: 
+#             max_counter = int(max(arr))
+#             arr = [max_counter]*N
+#     
+#         
+#     return arr
+#     
+#     
+#     pass
+# =============================================================================
 
 def solution(N, A):
     # write your code in Python 3.6
-
-
     arr = [0]*N
-
-
+    
+    max_counter = 0
     for i in A:
         if i <= N:
             arr[i-1] = arr[i-1]+1
-   
+            if arr[i-1] > max_counter:
+                max_counter = arr[i-1]
         else: 
-            max_counter = int(max(arr))
             arr = [max_counter]*N
-
-        arr = arr 
         
     return arr
 
