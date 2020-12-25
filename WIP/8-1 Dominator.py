e50d3846f4162e6b997c9f634a1b6c545d67c371
reejung kim <reejung.kim@gmail.com>
2020-12-25 00:03:46 +0000

    Add draft

---
+++ WIP/8-1 Dominator.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 25 00:01:52 2020
+
+@author: reejungkim
+"""
+
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A):
+    # write your code in Python 3.6
+    half = len(A)/2
+    dict = {}
+    #arr = []
+
+    for i in set(A):
+        count = A.count(i)
+        dict[i] = count
+        max_count = max( dict.values())
+    
+    #print(dict)
+    #print( max_count )
+    for key in dict.keys():
+        if dict[key] == max_count:
+            dominator = key
+
+
+    for i in range( len(A) ):
+        if A[i] ==  dominator:
+            answer = i
+            #arr.append(i)
+
+
+    if max_count > half:
+        solution = answer #arr
+    else:
+        solution = -1
+
+
+    return solution
+
+    pass
