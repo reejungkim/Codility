511c2fc36846e675c5e23cda4ad9b473e27bed64
reejung kim <reejung.kim@gmail.com>
2020-12-27 20:19:39 +0000

    Add solution comp O(N*M)

---
+++ 5-2 GenomicRangeQuery.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 27 20:08:43 2020
+
+@author: reejungkim
+"""
+
+def solution(S, P, Q):
+    # write your code in Python 3.6
+    dict = {'A':1, 'C':2, 'G':3, 'T':4}
+    S_converted = []
+    arr = []
+
+    for i in S:
+        S_converted.append(dict[i])
+
+    for i in range(0, len(P)):
+        start = P[i]
+        end = Q[i]
+        arr.append(min(S_converted[start: end+1]))
+
+    return arr
+
+    
+
+
+    pass
