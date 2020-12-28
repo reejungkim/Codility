9bc8c597ceefaf20fa4390286e0cd2c76e6dafe9
reejung kim <reejung.kim@gmail.com>
2020-12-28 12:19:57 +0000

    Update to O(1) complexity

--- 4-1 CountDiv.py
+++ 4-1 CountDiv.py
@@ -5,18 +5,10 @@ Created on Wed Dec 23 13:44:17 2020
 
 @author: reejungkim
 """
-
 def solution(A, B, K):
     # write your code in Python 3.6
 
-    arr=[]
-    for i in range(A, B+1):
- 
-        if i % K == 0:
-            arr.append(i)
-
-
-    return len(arr)
-
+    return B//K - (A-1)//K
+    
 
     pass
