85d874b2c35924ffe53942f84b475c69b106d5c5
reejung kim <reejung.kim@gmail.com>
2020-12-28 14:38:18 +0000

    Update to O(N+M)

--- 5-2 GenomicRangeQuery.py
+++ 5-2 GenomicRangeQuery.py
@@ -7,45 +7,22 @@ Created on Sun Dec 27 20:08:43 2020
 """
 
 
-
 def solution(S, P, Q):
     # write your code in Python 3.6
-    dict = {'A':1, 'C':2, 'G':3, 'T':4}
-    S_converted = []
-    arr = []
+    solutions  = []
+    for k in range(len(P)):
+        arr = S[P[k]: Q[k]+1]
+        if 'A' in arr:
+            solutions.append(1)
+        elif 'C' in arr:
+            solutions.append(2)
+        elif 'G' in arr:
+            solutions.append(3)
+        else:
+            solutions.append(4)
+                        
+    return solutions
+            
 
-    for i in S:
-        S_converted.append(dict[i])
-
-    for i in range(0, len(P)):
-        start = P[i]
-        end = Q[i]
-        arr.append(min(S_converted[start: end+1]))
-
-    return arr
-
-    
     pass
 
-
-# def solution(S, P, Q):
-#     # write your code in Python 3.6
-#     dict = {'A':1, 'C':2, 'G':3, 'T':4}
-    
-#     arr = []
-
-#     for i in range(0, len(P)):
-#         start = P[i]
-#         end = Q[i]
-#         #print(S[start: end+1])
-#         S_converted = []
-#         for n in S[start: end+1]:
-#             S_converted.append(dict[n])
-#             #print(S_converted)
-#         arr.append(min(S_converted))
-
-#     return arr
-
-
-
-#     pass
