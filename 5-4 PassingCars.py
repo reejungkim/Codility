4f95954c84a3ad4cf8e47f34bdc4f95bec65b13e
reejung kim <reejung.kim@gmail.com>
2020-12-31 14:56:50 +0000

    Update for better complexity

--- 5-4 PassingCars.py
+++ 5-4 PassingCars.py
@@ -5,36 +5,17 @@ Created on Thu Dec 24 00:11:50 2020
 
 @author: reejungkim
 """
-
 def solution(A):
-    # write your code in Python 3.6
-    arr_0 = []
-    arr_1 = []
 
-    for i in range(0, len(A)) :
-        if A[i] == 0:
-            arr_0.append(i)
-        else:
-            arr_1.append(i)
+     pairs = 0
+     zero_count = 0
 
-
-
-    count = 0
-
-    for x in arr_0:
-        for y in arr_1:
-            if x < y:
-                count +=1
-     
-    if count > 1000000000:
-        solution = -1
-    else:
-        solution = count
-
-    return solution
-   
-
-#    return -1
-
-
-    pass
+     for i in range(0, len(A)):
+         if A[i] == 0:
+             zero_count += 1
+         elif A[i] == 1:
+             pairs += zero_count
+             if pairs > 1000000000:
+                 return -1
+  
+     return pairs
