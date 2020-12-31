27892ebbd1b04d6f3e655c710fda14f1fe82d663
reejung kim <reejung.kim@gmail.com>
2020-12-31 12:05:36 +0000

    Update for better complexity

--- 3-2 Perm Missing Element.py
+++ 3-2 Perm Missing Element.py
@@ -6,17 +6,12 @@ Created on Tue Dec 22 22:00:42 2020
 @author: reejungkim
 """
 
+
 def solution(A):
     # write your code in Python 3.6
-    total = 0
-    length_A = len(A)
-    
-    for i in range(0, length_A) :
-        total += A[i] 
-    true_total = (length_A+1)*(length_A+1+1)/2
-    missing = true_total - total 
-    
-    return int(missing)
+    total = sum(A)
+    total_expected = (len(A)+2) * (len(A)+1) //2
 
+    return total_expected - total
 
     pass
