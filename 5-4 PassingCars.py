ee2f526f5ff4b1fc81ee0804ce4e6f4330187d3c
reejung kim <reejung.kim@gmail.com>
2021-01-01 10:05:07 +0000

    Update for better complexity

--- 5-4 PassingCars.py
+++ 5-4 PassingCars.py
@@ -6,16 +6,15 @@ Created on Thu Dec 24 00:11:50 2020
 @author: reejungkim
 """
 def solution(A):
-
-     pairs = 0
-     zero_count = 0
-
-     for i in range(0, len(A)):
-         if A[i] == 0:
-             zero_count += 1
-         elif A[i] == 1:
-             pairs += zero_count
-             if pairs > 1000000000:
-                 return -1
-  
-     return pairs
+    # write your code in Python 3.6
+    passing = 0
+    east = 0
+    for i, val in enumerate(A):
+        if val == 0: #east
+            east += 1
+        else: #west
+            passing += east
+            if passing > 1000000000:
+                return -1
+    return passing
+    pass
