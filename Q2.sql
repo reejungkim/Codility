320937394442263026357e267e77c687cb582753
reejung.kim <reejung.kim@gmail.com>
2025-01-09 21:42:55 +0900

    Add

--- Q2.sql
+++ Q2.sql
@@ -15,14 +15,18 @@ where row = 1
 and value2 is not null
 
 
-'''
-with rank_events as(
-    select event_type, value, time, rank() over(partition by event_type order by time desc) as rank
-    from events
-)
-select A.event_type, A.value - B.value as value
-from (select * from rank_events where rank=1) A
-join (select * from rank_events where rank=1) B 
-on A.event_type = B.event_type
-order by A.event type 
-'''
+
+
+-- SQLite submitted
+with x as(
+    SELECT event_type, 
+    value as latest, 
+    lead(value) over(partition by event_type order by time desc) as second_latest,
+    row_number() over(partition by event_type order by time desc) as row 
+    from events 
+) 
+select event_type ,
+latest - second_latest as value
+from x 
+where row = 1 and second_latest is not null 
+order by event_type
